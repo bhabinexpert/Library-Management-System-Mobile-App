256750f8ec49d68ca2a2ffe45d0bf3483b5dcee4
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-27 13:53:08 +0545

    screen header adjusted

--- LMS/app/_layout.jsx
+++ LMS/app/_layout.jsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+
 
 export default function RootLayout() {
   return (
     
     <Stack screenOptions={{ headerShown: false }} />
+    
       
     
   );
